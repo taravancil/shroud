ffa94187eb21b424e4258d7a3d0fe8e8173cd43f
Tara Vancil <tbvanc@gmail.com>
2016-11-29 23:01:16 -0500

    add tests

---
+++ test/test.js
@@ -0,0 +1,73 @@
+const fs = require('fs')
+const fse = require('fs-extra')
+const path = require('path')
+const test = require('ava')
+const error = require('../dist/error.js')
+const Shroud = require('../dist/index.js')
+
+const {DuplicateSecret, SecretNotFound} = error
+
+const TEST_MASTER_PASSWORD = 'verygoodA+password'
+const TEST_SECRET_OBJ = {'sekrit.com': 'sekrit'}
+
+function init (salt) {
+  // make a temp directory for each test's data files
+  const tmpDir = path.join(__dirname, 'tmp', Math.random(8).toString(16))
+  fs.mkdirSync(tmpDir)
+
+  return Shroud({
+    masterPassword: TEST_MASTER_PASSWORD,
+    dataDir: tmpDir,
+    salt: salt
+  })
+}
+
+test.before(t => {
+  fs.mkdirSync(path.join(__dirname, 'tmp'))
+})
+
+test.after.always(t => {
+  fse.removeSync(path.join(__dirname, 'tmp'))
+})
+
+test('add a duplicate secret to the vault', async t => {
+  const shroud = init()
+
+  await shroud.add(TEST_SECRET_OBJ)
+  const err = await t.throws(shroud.add(TEST_SECRET_OBJ))
+  t.true(err instanceof DuplicateSecret)
+})
+
+test('initialize shroud', async t => {
+  const shroud = init()
+  const data = shroud.data.read()
+  const keys = [
+    'masterKeySalt', 'masterPrivKeySalt', 'sealedMasterPrivKey', 'masterPubKey'
+  ]
+
+  // check that the object has values set for all of the keys
+  keys.forEach(key => t.truthy(data[key]))
+})
+
+test('add a secret to the vault', async t => {
+  const shroud = init()
+  const sealedSecretObj = await shroud.add(TEST_SECRET_OBJ)
+  const keys = ['sealedSecret', 'pubkey', 'salt']
+
+  // check that the object has values set for all of the keys
+  keys.forEach(key => t.truthy(sealedSecretObj[key]))
+})
+
+test('get a non-existent secret from the vault', async t => {
+  const shroud = init()
+
+  const err = await t.throws(shroud.reveal('sekrit.com'))
+  t.true(err instanceof SecretNotFound)
+})
+
+test('decrypt a secret', async t => {
+  const shroud = init()
+  await shroud.add(TEST_SECRET_OBJ)
+  const decrypted = await shroud.reveal('sekrit.com')
+  t.is(decrypted, TEST_SECRET_OBJ['sekrit.com'])
+})
